79bbb30c7556127e4312f7e93bcab875f6b3774a
Jason Gonzales <jasongonzo25@gmail.com>
2021-05-26 13:02:28 -0700

    complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
+import 'normalize.css';
 import App from './App';
+import {GlobalStyles} from './global-styles';
 import reportWebVitals from './reportWebVitals';
 
-render( <App />, document.getElementById('root')
+render(<>
+  <GlobalStyles />
+  <App />
+  </>,
+  document.getElementById('root')
 );
 
 
